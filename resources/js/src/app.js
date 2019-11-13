253127ee79a3bf9e92bcc1523bb712dd4810558a
Ryan Thompson <ryan@pyrocms.com>
2019-11-13 16:59:26 -0600

    Two Balls

--- resources/js/src/app.js
+++ resources/js/src/app.js
@@ -1,13 +1,9 @@
 var bootstrap = require('bootstrap.native/dist/bootstrap-native-v4');
 
-(function (window, document) {
+let messages = Array.prototype.slice.call(
+    document.querySelectorAll('[data-dismiss="toast"]')
+);
 
-    let messages = Array.prototype.slice.call(
-        document.querySelectorAll('.toast')
-    );
-
-    messages.forEach(function (message) {
-        new bootstrap.Toast(message).show();
-    });
-
-})(window, document);
+messages.forEach(function (message) {
+    message['Toast'].show();
+});
--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -2,89 +2,85 @@
 
     @if (app('messages')->has('success'))
         @foreach (app('messages')->pull('success') as $message)
-            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+            <div class="toast">
                 <div class="toast-header">
                     
-                    {{-- <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#007aff" width="100%" height="100%"></rect></svg> --}}
+                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#48bb78" width="100%" height="100%"></rect></svg>
 
-                    <strong class="mr-auto">Bootstrap</strong>
+                    <strong class="mr-auto">Success</strong>
                     {{-- <small>11 mins ago</small> --}}
                     
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" data-delay="3000">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="toast-body">
-                        {!! markdown(trans($message)) !!}
+                    {!! markdown(trans($message)) !!}
                 </div>
             </div>
-            {{-- <toaster :open="true" type="success" class="p-6 w-72 bg-green-500 text-green-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>   --}}
         @endforeach
     @endif
 
     @if (app('messages')->has('info'))
         @foreach (app('messages')->pull('info') as $message)
-            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+            <div class="toast">
                 <div class="toast-header">
                     
-                    {{-- <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#007aff" width="100%" height="100%"></rect></svg> --}}
+                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#4299e1" width="100%" height="100%"></rect></svg>
 
-                    <strong class="mr-auto">Bootstrap</strong>
+                    <strong class="mr-auto">Notice</strong>
                     {{-- <small>11 mins ago</small> --}}
                     
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" data-delay="3000">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="toast-body">
-                        {!! markdown(trans($message)) !!}
+                    {!! markdown(trans($message)) !!}
                 </div>
             </div>
-            {{-- <toaster :open="true" type="info" class="p-6 w-72 bg-blue-500 text-green-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>   --}}
         @endforeach
     @endif
 
     @if (app('messages')->has('warning'))
         @foreach (app('messages')->pull('warning') as $message)
-            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+            <div class="toast">
                 <div class="toast-header">
                     
-                    {{-- <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#007aff" width="100%" height="100%"></rect></svg> --}}
+                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#ed8936" width="100%" height="100%"></rect></svg>
 
-                    <strong class="mr-auto">Bootstrap</strong>
+                    <strong class="mr-auto">Warning</strong>
                     {{-- <small>11 mins ago</small> --}}
                     
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" data-delay="3000">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="toast-body">
-                        {!! markdown(trans($message)) !!}
+                    {!! markdown(trans($message)) !!}
                 </div>
             </div>
-            {{-- <toaster :open="true" type="warning" class="p-6 w-72 bg-orange-500 text-yellow-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>   --}}
         @endforeach
     @endif
 
     @if (app('messages')->has('error'))
         @foreach (app('messages')->pull('error') as $message)
-            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+            <div class="toast">
                 <div class="toast-header">
                     
-                    {{-- <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#007aff" width="100%" height="100%"></rect></svg> --}}
+                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#f56565" width="100%" height="100%"></rect></svg>
 
-                    <strong class="mr-auto">Bootstrap</strong>
+                    <strong class="mr-auto">Error</strong>
                     {{-- <small>11 mins ago</small> --}}
                     
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" data-delay="3000">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="toast-body">
-                        {!! markdown(trans($message)) !!}
+                    {!! markdown(trans($message)) !!}
                 </div>
             </div>
-            {{-- <toaster :open="true" type="error" class="p-6 w-72 bg-error-500 text-yellow-100 color-white rounded mr-3 mb-3" message="{{ markdown(trans($message)) }}"></toaster>   --}}
         @endforeach
     @endif
 
