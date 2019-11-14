affc2299206d57bc42a9094ca81f4c15ffb50840
Ryan Thompson <ryan@pyrocms.com>
2019-11-14 12:04:18 -0600

    Eh

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -39,17 +39,9 @@
             </div>
 
             <div class="absolute bottom-0 left-0 ml-2 mb-2 text-gray-500 text-xs fadeInUp animated delay-1s">
-                PYRO
+                {{ config('streams::distribution.name') }}
                 <span>{{ config('streams::distribution.version') }}</span>
             </div>
-
-            {{-- <div class="absolute bottom-0 right-0 mb-2 mr-2">
-        <a href="https://pyrocms.com/" class="p-2 bg-green-800 hover:bg-green-700 transition-bg items-center text-green-100 leading-none lg:rounded-full flex lg:inline-flex" target="_blank">
-            <span class="flex rounded-full bg-green-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
-            <span class="font-semibold mr-2 text-left flex-auto">v3.7 Released</span>
-            <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-            </a>
-            </div> --}}
 
         </div>
         <div class="login__right">
