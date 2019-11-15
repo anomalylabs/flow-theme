750fc293a8a5b894a263ec9b1f31d9490c980836
Ryan Thompson <ryan@pyrocms.com>
2019-11-15 09:58:41 -0600

    Ok

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,21 +1,5 @@
 <section id="messages" class="fixed bottom-0 right-0">
 
-    {{-- Why is this blinking off right away? --}}
-    <b-toast title="Success" visible static>
-        <p>Wtf? Why is this not persisting?</p>
-    </b-toast>
-
-    <template>
-    <div class="p-3 bg-secondary progress-bar-striped" style="min-height: 170px;">
-        <b-button class="mb-2" variant="primary" @click="$bvToast.show('example-toast')">
-        Show toast
-        </b-button>
-        <b-toast id="example-toast" title="BootstrapVue" static no-auto-hide>
-        Hello, world! This is a toast message.
-        </b-toast>
-    </div>
-    </template>
-
     @if (app('messages')->has('success'))
         @foreach (app('messages')->pull('success') as $message)
             <b-toast title="Success" visible static>
