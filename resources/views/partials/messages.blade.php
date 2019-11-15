c68368d10fd3faae70a4196929f654e4094cb278
Ryan Thompson <ryan@pyrocms.com>
2019-11-15 00:25:57 -0600

    K

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -5,6 +5,17 @@
         <p>Wtf? Why is this not persisting?</p>
     </b-toast>
 
+    <template>
+    <div class="p-3 bg-secondary progress-bar-striped" style="min-height: 170px;">
+        <b-button class="mb-2" variant="primary" @click="$bvToast.show('example-toast')">
+        Show toast
+        </b-button>
+        <b-toast id="example-toast" title="BootstrapVue" static no-auto-hide>
+        Hello, world! This is a toast message.
+        </b-toast>
+    </div>
+    </template>
+
     @if (app('messages')->has('success'))
         @foreach (app('messages')->pull('success') as $message)
             <b-toast title="Success" visible static>
