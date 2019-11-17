d7527a6ee74991b875bc55dba3e425d90f992afe
Ryan Thompson <ryan@pyrocms.com>
2019-11-16 18:16:32 -0600

    Boom

--- resources/js/src/app.js
+++ resources/js/src/app.js
@@ -6,3 +6,7 @@ Vue.use(BootstrapVue);
 var app = new Vue({
     el: '#app'
 });
+
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
+//             if (e.matches) console.log('your in dark mode);
+//             });
