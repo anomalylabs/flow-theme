77c879aa590d0fd31d6c0dc1478d557a35a71e53
Ryan Thompson <ryan@pyrocms.com>
2019-11-21 00:42:15 -0600

    Progress

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -5,11 +5,11 @@
         <span>{{ config('app.name') }}</span>
     </div>
 
-    <div class="topbar__toggle">
-        <button type="button" id="topbar__indicator -active">
+    <div class="topbar__toggle -closed">
+        <button type="button" class="topbar__indicator -open" onclick="document.querySelector('.topbar__navigation').classList.add('-open'); document.querySelector('.topbar__toggle').classList.add('-open'); return false;">
             {!! img('anomaly.theme.flow::img/menu-open.svg')->data() !!}
         </button>
-        <button type="button" id="topbar__indicator">
+        <button type="button" class="topbar__indicator" onclick="document.querySelector('.topbar__navigation').classList.remove('-open'); document.querySelector('.topbar__toggle').classList.remove('-open'); return false;">
             {!! img('anomaly.theme.flow::img/menu-close.svg')->data() !!}
         </button>
     </div>
