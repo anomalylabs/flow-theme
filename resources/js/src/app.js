0896354a3dfe51d5139996feac5dd9dbb2d365b5
Ryan Thompson <service@aiwebsystems.com>
2019-11-27 00:42:49 -0600

    Meh

--- resources/js/src/app.js
+++ resources/js/src/app.js
@@ -10,3 +10,35 @@ window.app = new Vue({
 // window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
 //     if (e.matches) console.log('your in dark mode);
 // });
+
+(function (window, document) {
+
+    let actions = Array.prototype.slice.call(
+        document.querySelectorAll('[data-keymap]')
+    );
+
+    actions.forEach(function (action) {
+
+        /**
+         * !:focus + ESC
+         */
+        Mousetrap.bind([action.dataset.keymap], function (event) {
+
+            if (!event.target.matches('body')) {
+                return;
+            }
+
+            event.preventDefault();
+
+            if (action.matches('input, textarea, select')) {
+
+                action.focus();
+
+                return;
+            }
+
+            action.click();
+        });
+
+    });
+})(window, document);
