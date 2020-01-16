a7185bcffabdfaddd1891fefea953488c03b7564
William <william@pixney.com>
2020-01-16 17:22:57 +0100

    Time to start configurating things for real.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      white:'#fff',
+      black:'#000'
+    },
+    fontFamily: {
+      display: ['JetBrains Mono', 'sans-serif'],
+      body: ['JetBrains Mono', 'sans-serif'],
+    },
     extend: {}
   },
   variants: {},
   plugins: []
 }
+
+
