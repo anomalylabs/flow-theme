84bb69c541a4af84ce1f6c6a6323d800b0cc9351
Ryan Thompson <ryan@pyrocms.com>
2020-01-29 11:04:29 -0600

    Clean up

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -8,11 +8,9 @@
 @endif
 
 @if (app('messages')->has('warning'))
-        <messages type="warning" :messages="{{ json_encode(app('messages')->pull('warning')) }}"></messages>
+    <messages type="warning" :messages="{{ json_encode(app('messages')->pull('warning')) }}"></messages>
 @endif
 
 @if (app('messages')->has('error'))
-        <messages type="error" :messages="{{ json_encode(app('messages')->pull('error')) }}"></messages>
+    <messages type="error" :messages="{{ json_encode(app('messages')->pull('error')) }}"></messages>
 @endif
- 
-
