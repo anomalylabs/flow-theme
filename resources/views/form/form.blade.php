dcfb9ae2e03e66a1454606824aabda676649f247
Ryan Thompson <ryan@pyrocms.com>
2020-01-28 21:44:14 -0600

    K

--- resources/views/form/form.blade.php
+++ resources/views/form/form.blade.php
@@ -1,15 +1,14 @@
 {{ assets('scripts.js', 'public::vendor/anomaly/core/js/form.js') }}
 
 @section('content')
-<code style='padding:.5rem 1rem; background:red; color:#fff'>~/Development/Web/streams/vendor/anomaly/flow-theme/resources/views/form/form.blade.php</code>
     <div class="form__wrapper">
-aaaaa
+
         {!! $form->open() !!}
         @include('admin::form/partials/controls')
         @include('admin::form/partials/heading')
         @include('admin::form/partials/layout')
         @include('admin::form/partials/controls')
         {!! $form->close() !!}
-x
+
     </div>
 @endsection
