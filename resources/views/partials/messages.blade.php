74e366897b07451f49ce57537276f0f848134801
Ryan Thompson <ryan@pyrocms.com>
2020-02-25 00:24:18 -0600

    Pull

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,16 +1,3 @@
-
-@if (app('messages')->has('success'))
-    <messages type="success" :messages="{{ json_encode(app('messages')->pull('success')) }}"></messages>
-@endif
-
-@if (app('messages')->has('info'))
-    <messages type="info" :messages="{{ json_encode(app('messages')->pull('info')) }}"></messages>
-@endif
-
-@if (app('messages')->has('warning'))
-    <messages type="warning" :messages="{{ json_encode(app('messages')->pull('warning')) }}"></messages>
-@endif
-
-@if (app('messages')->has('error'))
-    <messages type="error" :messages="{{ json_encode(app('messages')->pull('error')) }}"></messages>
-@endif
+@foreach (app('messages')->pull() as $message)
+<messages type="{{ $message['type'] }}" :messages="{{ json_encode([$message['content']]) }}"></messages>
+@endforeach
