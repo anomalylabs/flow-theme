71a2bf3cfd54ed41af4035e9527a4bcb4633648e
Ryan Thompson <ryan@pyrocms.com>
2020-03-11 22:26:36 -0500

    Streams

--- resources/views/buttons/buttons.blade.php
+++ resources/views/buttons/buttons.blade.php
@@ -1,15 +1,19 @@
-@foreach($buttons->primary() as $button)
-    {!! $button->open() !!}
-        {!! $button->icon() !!}
-        {{ $button->getText() }}
-    {!! $button->close() !!}
-@endforeach
+<buttons>
 
-@if (($secondary = $buttons->secondary())->isNotEmpty())
-    @foreach ($secondary as $button)
-    {!! $button->open() !!}
-        {!! $button->icon() !!}
-        {{ $button->getText() }}
-    {!! $button->close() !!}
-    @endforeach    
-@endif
+    @foreach($buttons->primary() as $button)
+        {!! $button->open() !!}
+            {!! $button->icon() !!}
+            {{ $button->getText() }}
+        {!! $button->close() !!}
+    @endforeach
+
+    @if (($secondary = $buttons->secondary())->isNotEmpty())
+        @foreach ($secondary as $button)
+        {!! $button->open() !!}
+            {!! $button->icon() !!}
+            {{ $button->getText() }}
+        {!! $button->close() !!}
+        @endforeach    
+    @endif
+
+</buttons>
