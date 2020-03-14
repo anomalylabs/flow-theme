75f59b35ead37de41f207db13c12a531fd1de6dd
Ryan Thompson <ryan@pyrocms.com>
2020-03-14 09:24:22 -0500

    Vue

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,33 +1,3 @@
 <aside id="sidebar">
-
-    <ul>
-        @foreach (cp()->getNavigation() as $key => $item)
-            <li data-slug="{{ $item->getSlug() }}">
-                
-                <a {!! html_attributes($item->attributes()) !!}>
-                    {!! $item->icon() !!}
-                    <span>{{ trans($item->getTitle()) }}</span>
-                </a>
-                
-                <ul class="<?php $item->isActive() ? 'active' : null; ?>">
-                    @foreach ($item->getSections()->root()->visible() as $section)
-                        <li>
-                            <a {!! html_attributes($section->attributes()) !!}>
-                                {!! $section->icon() !!}
-                                {{ $section->getTitle() }}
-
-                                @if ($section->getLabel())    
-                                    <span class="tag {{ $section->getContext() }}">
-                                        {{ $section->getLabel() }}
-                                    </span>
-                                @endif
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
-                
-            </li>
-        @endforeach
-    </ul>
-
+    <navigation :links="{{ cp()->getNavigation()->toJson() }}"></navigation>
 </aside>
