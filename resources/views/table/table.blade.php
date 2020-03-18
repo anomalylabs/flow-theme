7c05d1a630d6fe0de008ebd4836e2e71076b0116
Ryan Thompson <service@aiwebsystems.com>
2020-03-18 17:29:21 -0500

    slots

--- resources/views/table/table.blade.php
+++ resources/views/table/table.blade.php
@@ -1,21 +1,27 @@
 <cp-table :table="{{ $table->toJson() }}">
 
-    @include('admin::table/partials/filters')
-    {{-- @include('admin::table/partials/views') --}}
-    @include('admin::table/partials/heading')
+    <template v-slot:filters>
+        @include('admin::table/partials/filters')
+    </template>
+    
+    <template v-slot:heading>
+        @include('admin::table/partials/heading')
+    </template>
 
-    <div class="table__container">
-        @if ($table->hasRows())
-            {!! form_open(['url' => url()->full()]) !!}
-                <table {!! html_attributes($table->attributes()) !!}>
-                    @include('admin::table/partials/header')
-                    @include('admin::table/partials/body')
-                    @include('admin::table/partials/footer')
-                </table>
-            {!! form_close() !!}
-        @else
-            {{ trans('streams::message.no_results') }}
-        @endif
-    </div>
+    <template v-slot:table>
+        <div class="table__container">
+            @if ($table->hasRows())
+                {!! form_open(['url' => url()->full()]) !!}
+                    <table {!! html_attributes($table->attributes()) !!}>
+                        @include('admin::table/partials/header')
+                        @include('admin::table/partials/body')
+                        @include('admin::table/partials/footer')
+                    </table>
+                {!! form_close() !!}
+            @else
+                {{ trans('streams::message.no_results') }}
+            @endif
+        </div>
+    </template>
     
 </cp-table>
