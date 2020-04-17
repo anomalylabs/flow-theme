27e35a89324a9dc4a23de53f35c1058b21dcd39c
Ryan Thompson <service@aiwebsystems.com>
2020-04-17 01:55:03 -0500

    Just dump it to alert to keep it on mind.

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,3 +1,5 @@
-@foreach (app('messages')->pull() as $message)
-<messages type="{{ array_get($message, 'type', 'success') }}" :messages="{{ json_encode([$message['content']]) }}"></messages>
+@foreach (messages()->pull() as $message)
+<script>
+    alert('<messages type="{{ array_get($message, 'type', 'success') }}" :messages="{{ json_encode([$message['content']]) }}"></messages>');
+</script>
 @endforeach
